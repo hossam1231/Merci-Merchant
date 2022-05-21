0b00abd8438319b003cc450d7ccc8213883637f0
hossam1231 <hossamsulleman@gmail.com>
2022-05-21 21:42:18 +0100

    hey

--- my-app/components/search/SearchBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SearchBar = () => {
-  return (
-  <></>
-  )
-}
-
-export default SearchBar
--- my-app/screens/NotFoundScreen.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NotFoundScreen = () => {
-	return <></>;
-};
-
-export default NotFoundScreen;
